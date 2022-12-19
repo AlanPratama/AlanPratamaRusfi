cc4736f4bee0ccc2a985345d57221115f658995f
AlanPratama <109650474+AlanPratama@users.noreply.github.com>
2022-12-20 03:34:21 +0700

    Add files via upload

---
+++ FAV-MUSIC/js/music-list.js
@@ -0,0 +1,57 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+   
+    name: "Yasashii Suisei",
+    artist: "Yoasobi",
+    img: "comet",
+    src: "comet"
+  },
+  {
+    name: "Hilarious",
+    artist: "Charlie Puth",
+    img: "hilarious",
+    src: "hilarious"
+  },
+  {
+    name: "Somebody That I Used To Know",
+    artist: "Gotye",
+    img: "gotye",
+    src: "gotye"
+  },
+  {
+    name: "Stuck With You",
+    artist: "Ariana Grande & Justin Bieber",
+    img: "swu",
+    src: "swuu"
+  },
+  {
+    name: "Beauty And A Beat",
+    artist: "Justin Bieber & Nicki Minaj",
+    img: "batb",
+    src: "batb"
+  },
+  {
+    name: "Industry Baby x ET (Mashup)",
+    artist: "Lil Nas X & Katy Perry",
+    img: "lil",
+    src: "lil"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
---
+++ script.js
@@ -0,0 +1,60 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['WELCOME TO MY SELF :)', 'HOPE YOU LOVE IT', 'BECAUSE...', 'IT IS MADE WITH LOVE', 'AND...', '100% MADE BY MY SELF' ], 'text',['#f9fcfe','#f9fcfe','#f9fcfe','#f9fcfe','#f9fcfe','#f9fcfe']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+// 
